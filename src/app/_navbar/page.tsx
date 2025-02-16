2b41dedc5599ac89c65c591f81a2471f8cbe55c1
YousefHatem4 <youssefhatem242004@gmail.com>
2025-02-16 11:57:07 +0200

    update

---
+++ src/app/_navbar/page.tsx
@@ -0,0 +1,13 @@
+
+import { useEffect } from "react"
+
+
+export default function Navbar() {
+    useEffect(() => {
+        console.log('hello');
+        
+    } ,[])
+    return <>
+    <h1>navbar page</h1>
+    </>
+}
