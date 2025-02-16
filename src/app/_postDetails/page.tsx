ed519845733db384124111efd05218994704425f
YousefHatem4 <youssefhatem242004@gmail.com>
2025-02-16 12:50:00 +0200

    update

---
+++ src/app/_postDetails/page.tsx
@@ -0,0 +1,6 @@
+
+export default function PostDetails() {
+    return <>
+    <h1>PostDetails</h1>
+    </>
+}
---
+++ src/app/createpost/page.tsx
@@ -0,0 +1,6 @@
+
+export default function CreatePost() {
+    return <>
+    <h1>Add your Post</h1>
+    </>
+}
